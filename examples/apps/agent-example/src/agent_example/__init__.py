a68b03182bc722f7c6ea8da24e5846b32f10c6df
Leo Smerling <61629371+leosmerling-hopeit@users.noreply.github.com>
2025-09-19 13:12:58 +0200

    cleanup

--- examples/apps/agent-example/src/agent_example/__init__.py
+++ examples/apps/agent-example/src/agent_example/__init__.py
@@ -1,5 +1,5 @@
 """Agent example app."""
 
-from .settings import AgentSettings, load_agent_settings
+from .settings import AgentSettings
 
-__all__ = ["AgentSettings", "load_agent_settings"]
+__all__ = ["AgentSettings"]
